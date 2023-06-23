21a8a20ebffeb6494dabdc8f3868d1f48fea77fb
huhuiyu <1069306849@qq.com>
2023-06-23 11:24:47 +0800

    多仓库推送脚本

---
+++ push.ps1
@@ -0,0 +1,7 @@
+# git@github.com:huhuiyu/template-javaweb.git
+# git remote add jihulab git@jihulab.com:DarkKnight/template-javaweb.git
+# git remote add gitee git@gitee.com:huhuiyu/template-javaweb.git
+git pull
+git push
+git push jihulab
+git push gitee
